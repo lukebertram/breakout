2eb36240afe2cc59837e70b464d88be838685b95
Luke Bertram <lukebertram@gmail.com>
2017-11-29 22:39:28 -0800

    add ball movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-//RED SQUARE
-ctx.beginPath();
-//rect(topLeft_X, topLeft_Y, width, height)
-ctx.rect(65, 165, 125, 125);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
-//SOLID GREEN CIRCLE
-ctx.beginPath();
-//arc(center_X, center_Y, radius, startAngle_rad, endAngle_rad, direction)
-ctx.arc(350, 220, 77, 0, Math.PI*2, false);
-ctx.fillStyle = "#55CC88";
-ctx.fill();
-ctx.closePath();
+function draw() {
 
-//TRANSP. STROKE BLUE BOX
-ctx.beginPath();
-ctx.rect(160, 10, 250, 175);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.endPath();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
