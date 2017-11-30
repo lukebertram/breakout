8e783b42ff920a2f90c04f2425137f6e6ae098ed
Luke Bertram <lukebertram@gmail.com>
2017-11-29 23:03:07 -0800

    change ball color upon wall collision

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,37 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var newColor = "#000000"
 
-function drawBall() {
+function drawBall(color) {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
 
+function getRandomColor() {
+  var letters = '789ABCD';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 6)];
+  }
+  return color;
+}
 function draw() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
+  drawBall(newColor);
   x += dx;
   y += dy;
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
+    newColor = getRandomColor();
   }
   if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+    newColor = getRandomColor();
   }
 }
 
